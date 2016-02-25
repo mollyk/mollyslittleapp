6ae09ea0629d228ed58a22b14d766f0a195e2a74
mollyk <katsouli.mo@gmail.com>
2016-02-25 21:09:13 +0200

    Changed app.coffee to .js

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const app = module.exports = require("koa")();
+const http = require("http");
+const config = require("config");
+const port = config.get("app.port");
+
+// Initialize app context
+Object.assign(app.context, require("./context"));
+
+// Mount routes
+router = require("./routes");
+app.use(router.routes());
+app.use(router.allowedMethods());
+app.on("error", (err) => console.error(err));
+
+app.listen(port);
+console.log(`${config.get("app.name")} listening on ${port}`);
