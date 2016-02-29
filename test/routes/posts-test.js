a3ad378f2e34ea70136301ebd030646b13f931a2
mollyk <katsouli.mo@gmail.com>
2016-02-29 20:54:37 +0200

    Deleted Cakefile, created tasks, in process of pulling posts from db

--- test/routes/posts-test.js
+++ test/routes/posts-test.js
@@ -1,18 +1,21 @@
 const ctx = require("../context");
-const POSTS = require("../../data/posts.json");
+const config = require("config");
+ctx.db = require("../../lib/services/db")(config.get("db"), ctx);
+ctx.models = ctx.db.models;
+// const POSTS = require("../../data/posts.json");
 const assert = ctx.assert;
 
 
 describe("GET /posts", () => {
 	before(() => {
-		return ctx.models.Post.bulkCreate(POSTS,
+		return ctx.models.Post.bulkCreate(
 			{ ignoreDuplicates: true }
 		)
 	});
 
 	it("Brings back last 4 posts", () => {
 		return ctx.agent.get("/posts")
-		.then ( res => {
+		.then ( (res) => {
 			assert.equal(res.status, 200)
 			assert.ok(Array.isArray(res.body.posts), "posts is an array")
 			assert.ok((res.body.posts.length < 5), "Last 4 posts shown");
