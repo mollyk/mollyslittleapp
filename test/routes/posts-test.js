4e217894841f2a78e1a93b601f4264baf652e4d1
mollyk <katsouli.mo@gmail.com>
2016-03-02 20:00:39 +0200

    Created route GET /posts/:id

--- test/routes/posts-test.js
+++ test/routes/posts-test.js
@@ -4,21 +4,32 @@ ctx.db = require("../../lib/services/db")(config.get("db"), ctx);
 ctx.models = ctx.db.models;
 // const POSTS = require("../../data/posts.json");
 const assert = ctx.assert;
+const uuid = require("uuid");
 
 
 describe("GET /posts", () => {
-	before(() => {
-		return ctx.models.Post.bulkCreate(
-			{ ignoreDuplicates: true }
-		)
-	});
 
 	it("Brings back last 4 posts", () => {
 		return ctx.agent.get("/posts")
+		.query({ page: '1' })
 		.then ( (res) => {
 			assert.equal(res.status, 200)
 			assert.ok(Array.isArray(res.body.posts), "posts is an array")
 			assert.ok((res.body.posts.length < 5), "Last 4 posts shown");
+		});
+	});
+});
+
+describe("GET /posts/:id", () => {
+
+	it("Brings back a specific post", () => {
+		var id = uuid.v4();
+		return ctx.agent.get("/posts/${id}")
+		.then ( (res) => {
+			assert.equal(res.status, 200)
+			assert.ok((typeof res.body.post === "object") && (res.body.post !== null), "Post is object")
+			console.log(res.body.post);
+			assert.ok(Object.keys(res.body.post).length < 7 ,"One post shown");
 		});
 	});
 });
