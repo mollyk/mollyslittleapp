e85571ca73a3e7434a76c14ad828d070d602cb4f
mollyk <katsouli.mo@gmail.com>
2016-03-04 21:54:47 +0200

    Added Bootstrap

--- test/routes/posts-test.js
+++ test/routes/posts-test.js
@@ -5,8 +5,6 @@ ctx.models = ctx.db.models;
 const assert = ctx.assert;
 const uuid = require("uuid");
 
-
-
 describe("GET /posts", () => {
 
 	it("Brings back last 4 posts", () => {
@@ -35,12 +33,24 @@ describe("GET /posts/:id", () => {
 describe("POST /posts", () =>{
 	it("Creates new post", () => {
 		return ctx.agent.post("/posts")
-		.send({ title: 'New Post Title',
+		.send({ title: 'New Post Title 1',
 				content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' })
 		.then ( (res) => {
 			assert.ok(true, 'yeah')
 			assert.equal(res.status, 201)
 			assert.ok(res.body.new_post !== null, "The body is not null");
 		});
+	});
+});
+
+describe("PUT /posts/:id", () => {
+	var id = "1e659b39-d76a-4bdf-bea4-f48af6e86c88";
+	it("Edits existing post", () => {
+		return ctx.agent.put("/posts/${id}")
+		.send({ title: 'I changed my mind',
+				content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eius Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' })
+		.then ( (res) => {
+			assert.ok(res.status, 204)
+		})
 	});
 });
--- test/tasks/sync-db.js
+++ test/tasks/sync-db.js
@@ -3,7 +3,7 @@ const ctx = require("../lib/context");
 const co = require("co");
 const exec = require("child_process").exec();
 const config = require("config");
-ctx.db = require("../lib/services/db")(config.get("db"), ctx);
+ctx.db = require("../lib/services/db")(ctx);
 ctx.models = ctx.db.models;
 
 co(function *() {
@@ -16,23 +16,28 @@ co(function *() {
 	);
 	yield ctx.db.models.Post.bulkCreate([
 		{
+			id: "1e659b39-d76a-4bdf-bea4-f48af6e86c88",
 			title:"foobar",
 			content: "bazbarbazbarbazbar"
 		},
 		{
+			id: "9ea99667-202f-446c-adfd-588d1d5b269f",
 			title:"barbaz",
 			content: "foofoofoofoofoofoofoo"
 		},
 		{
+			id: "b1e438bd-1025-41dc-b14c-6e5612ab3d8c",
 			title:"bazfoo",
 			content: "barbarbarbarbarbarbar"
 		},
 		{
+			id: "9d4bb1c8-0a08-4082-b1a7-c3d547c05ec5",
 			title:"foobaz",
 			content: "barbarbarbar"
 		},
 		{
-				title:"barfoo",
+			id: "ba4ab14d-ff24-4e67-9fe4-393100e448dc",
+			title:"barfoo",
 			content: "bazbazbazbazbazbazbaz"
 		}], {ignoreDuplicates: true}
 	);
